f57ec11b0d22a637da474c7ab20f3f15a63da6f9
stollz-h4 <sm@hfour.com>
2020-12-04 18:51:25 +0100

    js fundamentals part 1 - coding challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -50,7 +50,7 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
 */
-
+/*
 const now = 2020;
 const ageJonas = now - 1991;
 const ageSarah = now - 2002;
@@ -65,3 +65,45 @@ console.log(x, y);
 
 const avgAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, avgAge);
+*/
+
+// Coding challenge 1
+/*
+Mark and John are trying to compare their BMI which is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula
+3. Create a boolean variable markHigherBMI containing information about whether Mark has
+a higher BMI than John
+
+TEST DATA 1: Mark weights 78kg and is 1.69m tall. John weights 92kg and is 1.95m tall.
+TEST DATA 2: Mark weights 95kg and is 1.88m tall. John weights 85kg and is 1.76m tall.
+*/
+
+
+let heightMark, heightJohn, weightMark, weightJohn, bmiMark, bmiJohn, markHigherBMI;
+
+heightMark = 1.69;
+weightMark = 78;
+heightJohn = 1.95;
+weightJohn = 92;
+
+bmiMark = weightMark / heightMark ** 2;
+bmiJohn = weightJohn / heightJohn ** 2;
+
+markHigherBMI = bmiMark > bmiJohn;
+
+console.log("Test case 1: ", bmiMark, bmiJohn, markHigherBMI);
+
+heightMark = 1.88;
+weightMark = 95;
+heightJohn = 1.76;
+weightJohn = 85;
+
+bmiMark = weightMark / heightMark ** 2;
+bmiJohn = weightJohn / heightJohn ** 2;
+
+markHigherBMI = bmiMark > bmiJohn;
+
+console.log("Test case 2: ", bmiMark, bmiJohn, markHigherBMI);
