186080ddd4b34e1f30dd0d065cddc2173de851f2
stollz-h4 <sm@hfour.com>
2020-12-04 11:57:34 +0100

    js fundamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,67 @@
+/*
+let country = "Macedonia";
+let continent = "Europe";
+let population = 2000000;
+*/
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof "315");
+console.log(typeof 315);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+*/
+
+/*
+const now = 2020;
+const ageJonas = now - 1991;
+const ageSarah = now - 2002;
+
+console.log(ageJonas, ageSarah);
+console.log(ageSarah * 2, ageJonas / 2, ageJonas ** 3);
+
+// concatenate strings
+const firstName = "Stojan";
+const lastName = "Mihajlovski";
+console.log(firstName + " " + lastName);
+
+// assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah); // <, >, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2020;
+const ageJonas = now - 1991;
+const ageSarah = now - 2002;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const avgAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avgAge);
