a283b91bd684bd88e18fa919d1ce6961de27304f
stollz-h4 <sm@hfour.com>
2020-12-06 16:32:36 +0100

    js fundamentals part 1 - coding challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -81,7 +81,7 @@ TEST DATA 1: Mark weights 78kg and is 1.69m tall. John weights 92kg and is 1.95m
 TEST DATA 2: Mark weights 95kg and is 1.88m tall. John weights 85kg and is 1.76m tall.
 */
 
-
+/*
 let heightMark, heightJohn, weightMark, weightJohn, bmiMark, bmiJohn, markHigherBMI;
 
 heightMark = 1.69;
@@ -107,3 +107,75 @@ bmiJohn = weightJohn / heightJohn ** 2;
 markHigherBMI = bmiMark > bmiJohn;
 
 console.log("Test case 2: ", bmiMark, bmiJohn, markHigherBMI);
+*/
+
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2020;
+
+const jonas = 'I\'m ' + firstName + ', a ' + (2020 - birthYear) + ' year old ' + job;
+console.log(jonas);
+
+// Template literal using backticks
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(jonasNew);
+
+// Using backticks for multiple line strings
+// --old way
+console.log('String with \n\ multiple \n\ lines');
+// --using backticks
+console.log(`Multiple line
+string
+using
+backticks`);
+*/
+
+/*
+const age = 17;
+
+if (age >= 18) {
+	console.log('Sarah can start driving 🚗');
+} else {
+	const yearsLeft = 18 - age;
+	console.log(`Sarah has ${yearsLeft} ${yearsLeft == 1 ? 'year' : 'years'} left before she can drive 🤷‍♀️`);
+}
+
+const birthYear = 1991;
+let century;
+
+if(birthYear <= 2000) {
+	century = 20;
+} else {
+	century = 21;
+}
+
+console.log(`You were born in the ${century} century`);
+*/
+
+// Coding challenge 2
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+Print a nice output to the console, saying who has the higher BMI. The message can be either
+"Mark's BMI is higher than John's" or the other way around
+*/
+
+let heightMark, heightJohn, weightMark, weightJohn, bmiMark, bmiJohn, markHigherBMI;
+
+heightMark = 1.69;
+weightMark = 78;
+heightJohn = 1.95;
+weightJohn = 92;
+
+bmiMark = weightMark / heightMark ** 2;
+bmiJohn = weightJohn / heightJohn ** 2;
+
+markHigherBMI = bmiMark > bmiJohn;
+
+if(markHigherBMI) {
+	console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn}).`);
+} else {
+	console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark}).`);
+}
