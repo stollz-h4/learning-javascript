232bb957a4f530aebcf391a75e60cdd96938f643
stollz-h4 <sm@hfour.com>
2020-12-22 16:22:11 +0100

    Coding challenge #3
    Completed the third challenge from Section 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -162,6 +162,7 @@ Print a nice output to the console, saying who has the higher BMI. The message c
 "Mark's BMI is higher than John's" or the other way around
 */
 
+/*
 let heightMark, heightJohn, weightMark, weightJohn, bmiMark, bmiJohn, markHigherBMI;
 
 heightMark = 1.69;
@@ -178,4 +179,145 @@ if(markHigherBMI) {
 	console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn}).`);
 } else {
 	console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark}).`);
+}
+*/
+
+
+/*
+// Type conversion
+let inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+// Type coercion
+console.log('I am ' + 23 + ' years old'); // will produce string
+console.log('23' - '10' - 3); // will produce number
+console.log('23' * '2'); // will produce number
+console.log('23' > '18'); // will produce number
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n); //10
+*/
+
+
+/*
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean("random"));
+console.log(Boolean({}));
+
+const money = 0;
+if(money) {
+	console.log(`Don't spend it all`);
+} else {
+	console.log(`You should get a job`); // This block will be executed because money = 0 is a falsy value
+}
+
+let height;
+if(height) {
+	console.log(`Yey, height is defined!`);
+} else {
+	console.log(`Seems like height is not yet defined`);
+}
+*/
+
+/*
+const age = 18;
+if(age === 18) console.log(`You just became an adult`); 
+
+const favorite = Number(prompt('Whats your favorite number?'));
+console.log(favorite);
+console.log(typeof favorite);
+
+if(favorite === 23) {
+	console.log('Cool! 23 is an amazing number!');
+} else if (favorite === 7) {
+	console.log('7 is also a cool number');
+} else {
+	console.log('Number is not 23 or 7');
+}
+
+if (favorite !== 23) console.log('Why not 23?');
+*/
+
+/*
+const hasDriversLicence = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+// if(hasDriversLicence && hasGoodVision) {
+// 	console.log('Sarah is able to drive!');
+// } else {
+// 	console.log('Someone else should drive!');
+// }
+
+const isTired = false;
+console.log(hasGoodVision || hasDriversLicence || isTired);
+
+if(hasDriversLicence && hasGoodVision && !isTired) {
+	console.log('Sarah is able to drive!');
+} else {
+	console.log('Someone else should drive!');
+}
+*/
+
+// Coding challenge 3
+/*
+There are two gymnastics teams, Dolphins and Koalas. 
+They compete against each other 3 times. The winner 
+with the highest average score wins the trophy.
+
+1. Calculate the average score for each team, using the test data below.
+
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test 
+for that as well.
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, 
+a team only wins if has a higher score than the other team, and a score of at 
+least 100 points at the same time. HINT: Use a logical operator to test for minimum score,
+as well as multiple else-if blocks.
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal to 100.
+Otherwise, no team wins.
+
+TEST DATA:
+Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110.
+
+TEST DATA BONUS 1:
+Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123.
+
+TEST DATA BONUS 2:
+Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+// calculate average score for teams
+const scoreDolphins = (100 + 108 + 102) / 3;
+const scoreKoalas = (100 + 108 + 102) / 3;
+
+// determine winner
+if(scoreDolphins > 100 || scoreKoalas > 100) {
+	if(scoreDolphins > scoreKoalas) {
+		console.log(`The Dolphins won! Dolphins ${scoreDolphins} : Koalas ${scoreKoalas}`);
+	} else if (scoreKoalas > scoreDolphins) {
+		console.log(`The Koalas won! Koalas ${scoreKoalas} : Dolphins ${scoreDolphins}`);
+	} else if (scoreKoalas === scoreDolphins) {
+		console.log(`The match ended in a draw`);
+	} else {
+		console.log(`Something is wrong`);
+	}
+} else {
+	console.log(`Neither team won at least a 100 points, which is the minimum`);
 }
