8278625b4bed1f4055fa6c64109101f780af5173
stollz-h4 <sm@hfour.com>
2021-01-18 15:46:05 +0100

    section 3, coding challenge 1 (functions)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 let country = "Macedonia";
 let continent = "Europe";
@@ -410,7 +411,138 @@ TEST DATA: Test for bill values 275, 40 and 430.
 
 // console.log(check);
 
+/*
 const bill = 40;
 let tip = (bill < 50 || bill > 300) ? .2 : .15;
 let check = bill + bill * tip;
 console.log(`The bill = ${bill}, the tip = ${tip} and the total check = ${check}`);
+*/
+
+/*
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriverLicence = true;
+if (hasDriversLicence) console.log('I can drive 🚗');
+*/
+
+// function logger() {
+// 	console.log(`It's me, Muerte`);
+// }
+
+// logger();
+
+/*
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+*/
+
+/*
+// function declaration
+function calcAge1(birthYear) {
+	// const age = 2021 - birthYear;
+	// return age;
+	return 2020 - birthYear;
+}
+
+const age1 = calcAge1(1985);
+
+// function expression
+const calcAge2 = function(birthYear) {
+	return 2020 - birthYear;
+}
+
+const age2 = calcAge2(1985);
+
+console.log(age1, age2); 
+*/
+
+/*
+// arrow function
+const calcAge3 = birthYear => 2020 - birthYear;
+const age3 = calcAge3(1985);
+console.log(age3);
+
+const yearsUntilRetirement = (firstName, birthYear) => {
+	const age = 2021 - birthYear;
+	const retirement = 64 - age;
+	// return retirement;
+	return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement('Stojan', 1985));
+*/
+
+/*
+function cutFruitPieces(fruit) {
+	return fruit * 4;
+}
+
+// call function inside another
+function fruitProcessor(apples, oranges) {
+	const applePieces = cutFruitPieces(apples);
+	const orangePieces = cutFruitPieces(oranges);
+	console.log(apples, oranges);
+	const juice = `Juice with ${applePieces} apples pieces and ${orangePieces} orange pieces`;
+	return juice;
+}
+
+console.log(fruitProcessor(3, 4));
+*/
+
+/*
+const calcAge = function(yearBirth) {
+	return 2021 - yearBirth;
+}
+
+const yearsUntilRetirement = function (firstName, birthYear) {
+	const age = calcAge(birthYear);
+	const retirement = 64 - age;
+	return retirement <= 0 ? 'already retired' : retirement;
+	// return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement('Stojan', 1950));
+*/
+
+// Coding challenge 2.1
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas. There is a new gymnastics discipline,
+which works differently. 
+Each team competes 3 times, and then the average of the 3 scores is calculated (so 1 average
+score per team). 
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores. 
+2. Use the function to calculate the average for both teams.
+3. Create a function 'checkWinner' that takes the average score of each team as parameters and then
+logs the winner to the console, together with the victory points, according to the rule above. 
+Example: "Koalas win (30 vs 13)"
+4. Use the checkWinner function to determine the winner for both DATA 1 and DATA 2
+5. Ignore draws this time
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49. 
+TEST DATA 2: Dolphins score 85, 54 an 41. Koalas score 23, 34 and 27.
+*/
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+const avgDolphins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolphins, avgKoalas) {
+	if (avgDolphins > avgKoalas * 2) {
+		return `Dolphins win! (${avgDolphins} vs ${avgKoalas})`;
+	} else if (avgKoalas > avgDolphins * 2) {
+		return `Koalas win! (${avgKoalas} vs ${avgDolphins})`;	
+	} else {
+		return `There is no winnner at this moment`;
+	}
+}
+
+console.log(checkWinner(avgDolphins, avgKoalas));
