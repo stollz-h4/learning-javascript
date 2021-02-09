0c147e266e40ad2d36292a2af149018b7e784dad
stollz-h4 <sm@hfour.com>
2021-02-09 15:40:09 +0100

    modal window exercise

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+const hideModal = function() {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+}
+
+const showModal = function() {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+}
+
+for(let i = 0; i < btnShowModal.length; i++) {
+	btnShowModal[i].addEventListener("click", showModal);
+};
+
+btnCloseModal.addEventListener("click", hideModal);
+overlay.addEventListener("click", hideModal);
+
+document.addEventListener("keydown", function(e) {
+	if(e.key === "Escape" && !modal.classList.contains("hidden")) {
+		hideModal();
+	}
+});
